ef11c5936f856d4a65d24c41e5771628b94686ef
Mariusz Gęzicki <mariusz.v7@gmail.com>
2015-05-06 19:26:22 +0200

    Add WebSocket handshake procedure.
    To open a WebSocket, the handshake is required.

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import threading
 import select
+import re
+import hashlib
+import base64
 
 
 class Client(object):
@@ -15,8 +18,42 @@ class Client(object):
 
         self.socket.setblocking(0)
 
+        self.handshake_start = False
+        self.handshake_completed = False
+
+    def handshake(self, command):
+        if not self.handshake_start:
+            check = re.match('GET', command)
+            if check:
+                self.handshake_start = True
+        else:
+            check = re.match(r'Sec-WebSocket-Key: (.*?)$', command, re.M | re.I)
+            if check:
+                wskey = str(check.group(1)).strip()
+                wskey += str('258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
+
+                response_code = base64.b64encode(hashlib.sha1(wskey).digest())
+
+                response = 'HTTP/1.1 101 Switching Protocols\r\n'
+                response += 'Upgrade: websocket\r\n'
+                response += 'Connection: Upgrade\r\n'
+                response += 'Sec-WebSocket-Accept: '+response_code+'\r\n'
+                response += '\r\n'
+
+                self.send(response)
+                self.handshake_start = False
+                self.handshake_completed = True
+
     def on_receive(self, data):
-        pass
+        if not self.handshake_completed:
+            lines = data.split('\n')
+            for command in lines:
+                self.handshake(command)
+        else:
+            self.data_from_websocket(data)
+
+    def data_from_websocket(self, data):
+        print 'ws: ', data
 
     def send(self, data):
         self.socket.sendall(data)
